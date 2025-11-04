4a135aaff3546d38ecf754f99a0c63ba1199c7e9
JonathanMazzoli2401 <jonathanmazzoli25@gmail.com>
2025-11-04 19:19:23 -0300

    C# code week 2

---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+public class Resumes
+{
+    public string _name;
+
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine(_name);
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
