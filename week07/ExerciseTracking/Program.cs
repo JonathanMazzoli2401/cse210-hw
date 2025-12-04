6e57073bfd519e33d91bf7f4053b0f41686ece7a
JonathanMazzoli2401 <jonathanmazzoli25@gmail.com>
2025-12-04 16:24:43 -0300

    W07 Excercise Tracking Program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2024, 11, 3), 30, 3.0),
+            new Cycling(new DateTime(2024, 11, 4), 45, 15.0),
+            new Swimming(new DateTime(2024, 11, 5), 25, 20)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
